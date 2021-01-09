069bd14d9706bce47ed84ec9d9f9ff375fd8d92a
Minecraftchest1 <Wyattcjackson2006@outlook.com>
2021-01-09 22:24:11 +0000

    Commit changes from local dev.
    Add group varible. Add logging.

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	)
+        "fmt"
+        "log"
+        "io/ioutil"
+        "net/http"
+        )
 
 func main() {
     //fmt.Println("Hello World")
 
-	token	:= "<Groupme Token>"
+        token   := "<token>"
+        group   := "<group>"
 
-	resp, err  := http.Get("https://api.groupme.com/v3/groups/65225076?token=" + token)
+        resp, err  := http.Get("https://api.groupme.com/v3/groups/" + group + "?token=" + token)
 
-	msgBody, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	fmt.Printf("%s", msgBody)
-	fmt.Println(err)
+        msgBody, err := ioutil.ReadAll(resp.Body)
+        resp.Body.Close()
+        fmt.Printf("%s", msgBody)
+        if err != nil {
+                log.Fatal(err)
+        }
 }
