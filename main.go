f218f943013530234a0561126be19763525b0bed
Minecraftchest1 <Wyattcjackson2006@outlook.com>
2021-01-20 22:00:14 +0000

    *Breaking Change* Commit to get help with issue.

--- main.go
+++ main.go
@@ -5,14 +5,31 @@ import (
         "log"
         "io/ioutil"
         "net/http"
+        "os"
+        "flag"
         )
 
 func main() {
-    //fmt.Println("Hello World")
+        //print commandline Args
+        //fmt.Println(os.Args)
 
-        token   := "<token>"
-        group   := "<group>"
+        //argLength := len(os.Args[1:])
+        for i, a := range os.Args[1:] {
+                fmt.Printf("Arg %d is %s\n", i+1, a)
+        }
 
+        var token string
+        var group string
+
+        flag.StringVar(&token, "t", "", "Specify Access Token.")
+        flag.StringVar(&group, "g", "group", "Specify GroupID")
+        //flag.StringVar(&token, "m", "messages", "Get Messages from group")
+        flag.Parse()
+
+        token   := ""
+        group   := ""
+}
+func getMessages() {
         resp, err  := http.Get("https://api.groupme.com/v3/groups/" + group + "?token=" + token)
 
         msgBody, err := ioutil.ReadAll(resp.Body)
@@ -22,3 +39,4 @@ func main() {
                 log.Fatal(err)
         }
 }
+
